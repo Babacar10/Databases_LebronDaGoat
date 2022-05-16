ea49a433af068df55861ff68971a898072ecbbae
Babacar10 <Diattafive@gmail.com>
2022-05-16 12:59:50 -0400

    adding querys

--- NBAHistoricStatsS3G6/index.js
+++ NBAHistoricStatsS3G6/index.js
@@ -1,4 +1,11 @@
-
+//var rhit = rhit || {};
+const adminApiUrl = "http://localhost:3000/";
+//Reference (Note: the Admin api tells you words.  You are an admin.):
+// POST   /api/admin/add      with body {"word": "..."} - Add a word to the word list
+// GET    /api/admin/words    													- Get all words
+// GET    /api/admin/word/:id 													- Get a single word at index
+// PUT    /api/admin/word/:id with body {"word": "..."} - Update a word at index
+// DELETE /api/admin/word/:id 	
 const express = require('express'); 
 const dotenv = require('dotenv'); 
 const app = express(); 
@@ -14,13 +21,24 @@ app.get('/', (req, res) => {
 
 });
 
+
 app.use('/api/DatabaseConnection', require('./api/DatabaseConnection'));
 
 app.listen(process.env.PORT, () => {
 
     console.log(`Server running on ${process.env.PORT} for ${process.env.NODE_ENV}`);
 
-})
+});
+
+
 
 module.exports = app;
+
+
+// new AdminController();
+// console.log("hello");
+	
+
+
+
 
--- NBAHistoricStatsS3G6/public/scripts/main.js
+++ NBAHistoricStatsS3G6/public/scripts/main.js
@@ -1,40 +1,84 @@
+var rhit = rhit || {};
 const apiURL = "http://localhost:3000/api/DatabaseConnection" 
 let allEntries = fetch(apiURL)
         .then(response => response.json())
         .then(data => console.log(data)); 
 
+        rhit.AdminController = class {
+                constructor() {
+                console.log("construct");
+                document.querySelector("#getall").onclick = (event) => {
+                                this.readAll();
+                        };
+                document.querySelector("#getone").onclick = (event) => {
+                                this.readone();
+                        };
+                        document.querySelector("#del").onclick = (event) => {
+                                this.deleteone();
+                        };
+                        document.querySelector("#update").onclick = (event) => {
+                                this.editone();
+                        };
+                        document.querySelector("#create").onclick = (event) => {
+                                this.createone();
+                        };
+            }
+            readAll() {
+                console.log(`Reading Players`);
+            
+                // TODO: Add your code here.
+                //document.querySelector("#getone").innerHTML = "";
+                
+                fetch(`http://localhost:3000/api/DatabaseConnection`).then(response => response.json()).then(words => {
+                    // let i = 0
+                    // while (i < words["words"].length) {
+                    //     console.log(words);
+                    // }
+                    console.log(words);
+                    
+                    
+                })
+                // console.log(words);
+                // Hint for something you will need later in the process (after backend call(s))
+                
+            }
+            readone() {
+                console.log(`Reading Players`);
+                let pid = document.getElementById("getoneid").value;
+                // TODO: Add your code here.
+                //document.querySelector("#getone").innerHTML = "";
+                
+                fetch("http://localhost:3000/api/DatabaseConnection/:id/?" + "playerid="+ pid).then(response => response.json()).then(words => {
+                    let i = 0
+                //     while (i < words.length) {
+                //         console.log(words);
+                //     }
+                    console.log(words);
+                    var array = words[0];
+                    document.getElementById("display-array").textContent = array.Name +" "+ array.Height +" "+ array.Weight;
+                })
+                // console.log(words);
+                // Hint for something you will need later in the process (after backend call(s))
+                
+            }
 
+            deleteone(){
+                console.log(`Deleting Player`);
+                let pid = document.getElementById("getdelid").value;
+                       
 
-document.getElementById("submit").onclick = (event) => {
-        let nameVal = document.getElementById("1").value;
-        let heightVal = document.getElementById("2").value;
-        let weight = document.getElementById("3").value;
-        let position = document.getElementById("4").value;
-        let hof = document.getElementById("5").value;
-        let yearborn =  document.getElementById("6").value;
-
-
-        const response = fetch("http://localhost:3000/api/DatabaseConnection/?" + "name="+ nameVal + "&height=" + heightVal + "&weight=" + weight + "&position=" + position + "&hof=" + hof + "&yearborn=" +yearborn, 
-        {
-        method: 'POST'
-        });
-
-        console.log("Player "+nameVal+" added");
-}
-
-document.getElementById("del").onclick = (event) => {
-        let pid = document.getElementById("delnum").value;
-
-        const response = fetch("http://localhost:3000/api/DatabaseConnection/:id/?" + "playerid="+ pid,
-        {
-        method: 'DELETE'
+                const response = fetch("http://localhost:3000/api/DatabaseConnection/:id/?" + "playerid="+ pid,
+                {
+                method: 'DELETE'
         });
 
         console.log("Player "+pid+" deleted");
-}
 
-document.getElementById("edit").onclick = (event) => {
-        let pid = document.getElementById("10").value;
+            }
+
+
+        editone(){
+                        let pid = document.getElementById("10").value;
         let name = document.getElementById("11").value;
         let height = document.getElementById("12").value;
         let weight = document.getElementById("13").value;
@@ -49,17 +93,88 @@ document.getElementById("edit").onclick = (event) => {
         });
 
         console.log("Player "+pid+" edited");
-}
+        }
 
-document.getElementById("getone").onclick = (event) => {
-        let pid = document.getElementById("getoneid").value;
+        createone(){
+                let pid = document.getElementById("10").value;
+        let nameVal = document.getElementById("11").value;
+        let heightVal = document.getElementById("12").value;
+        let weight = document.getElementById("13").value;
+        let position = document.getElementById("14").value;
+        let hof = document.getElementById("15").value;
+        let yearborn =  document.getElementById("16").value;
+        const response = fetch("http://localhost:3000/api/DatabaseConnection/?" + "name="+ nameVal + "&height=" + heightVal + "&weight=" + weight + "&position=" + position + "&hof=" + hof + "&yearborn=" +yearborn, 
+                {
+                method: 'POST'
+                });
+        
+                console.log("Player "+nameVal+" added");
+        }
+        
+        }
+// document.getElementById("submit").onclick = (event) => {
+//         let nameVal = document.getElementById("1").value;
+//         let heightVal = document.getElementById("2").value;
+//         let weight = document.getElementById("3").value;
+//         let position = document.getElementById("4").value;
+//         let hof = document.getElementById("5").value;
+//         let yearborn =  document.getElementById("6").value;
 
 
-        const response = fetch("http://localhost:3000/api/DatabaseConnection/:id/?" + "playerid="+ pid ,
-        {
-        method: 'GET'
-        })
-        .then(response => response.json())
-        .then(data => console.log(data));
+//         const response = fetch("http://localhost:3000/api/DatabaseConnection/?" + "name="+ nameVal + "&height=" + heightVal + "&weight=" + weight + "&position=" + position + "&hof=" + hof + "&yearborn=" +yearborn, 
+//         {
+//         method: 'POST'
+//         });
+
+//         console.log("Player "+nameVal+" added");
+// }
+
+// document.getElementById("del").onclick = (event) => {
+//         let pid = document.getElementById("delnum").value;
+
+//         const response = fetch("http://localhost:3000/api/DatabaseConnection/:id/?" + "playerid="+ pid,
+//         {
+//         method: 'DELETE'
+//         });
+
+//         console.log("Player "+pid+" deleted");
+// }
+
+// document.getElementById("edit").onclick = (event) => {
+//         let pid = document.getElementById("10").value;
+//         let name = document.getElementById("11").value;
+//         let height = document.getElementById("12").value;
+//         let weight = document.getElementById("13").value;
+//         let position = document.getElementById("14").value;
+//         let hof = document.getElementById("15").value;
+//         let yearborn =  document.getElementById("16").value;
+
+
+//         const response = fetch("http://localhost:3000/api/DatabaseConnection/:id/?" + "playerid="+ pid +"&name="+ name + "&height=" + height + "&weight=" + weight + "&position=" + position + "&hof=" + hof + "&yearborn=" +yearborn,
+//         {
+//         method: 'PUT'
+//         });
+
+//         console.log("Player "+pid+" edited");
+// }
+
+// document.getElementById("getone").onclick = (event) => {
+//         let pid = document.getElementById("getoneid").value;
+
+
+//         const response = fetch("http://localhost:3000/api/DatabaseConnection/:id/?" + "playerid="+ pid ,
+//         {
+//         method: 'GET'
+//         })
+//         .then(response => response.json())
+//         .then(data => console.log(data));
        
-}
+// }
+/* Main */
+rhit.main = function () {
+	
+new rhit.AdminController();
+	
+};
+
+rhit.main();
