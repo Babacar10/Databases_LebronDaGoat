760a4deae6da887453771466f522d2d0b5d011ba
Babacar10 <Diattafive@gmail.com>
2022-05-17 16:59:05 -0400

    added pages and made pagetwo homepage

--- NBAHistoricStatsS3G6/public/scripts/main.js
+++ NBAHistoricStatsS3G6/public/scripts/main.js
@@ -4,6 +4,101 @@ let allEntries = fetch(apiURL)
         .then(response => response.json())
         .then(data => console.log(data)); 
 
+
+
+
+        rhit.compareController = class {
+                constructor() {
+                                console.log("hello");
+                                document.querySelector("#compare").onclick = (event) => {
+                                        this.readcompareone();
+                                        this.readcomparetwo();
+                                }
+                    }
+                    
+            readcompareone(){
+                console.log(`Reading Players`);
+                let pid = document.getElementById("30").value;
+                // TODO: Add your code here.
+                //document.querySelector("#getone").innerHTML = "";
+                
+                fetch("http://localhost:3000/api/DatabaseConnection/:id/?" + "playerid="+ pid).then(response => response.json()).then(data => {
+                    let i = 0
+                //     while (i < words.length) {
+                //         console.log(words);
+                //     }
+                    //console.log(words);
+                    //var array = words[0];
+                    //document.getElementById("display-array").textContent = array.Name +" "+ array.Height +" "+ array.Weight;
+                    var table = document.getElementById('tableBody1');
+                    table.innerHTML ='';
+console.log(data);
+let dataHtml = ''
+data.forEach(element => {
+        let hof = "No"
+        if (element['HOF']){
+                hof = "Yes"
+        }
+        dataHtml += `<tr><td>${element['Name']}</td><td>${Math.floor(element['Height'] / 12)}' ${element['Height'] - Math.floor(element['Height'] / 12)*12}"</td><td>${element['Weight']}</td><td>${element['Position']}</td><td>${hof}</td></tr>`
+});
+table.innerHTML = dataHtml;
+                })
+                // console.log(words);
+                // Hint for something you will need later in the process (after backend call(s))
+                
+            
+
+            }
+            readcomparetwo(){
+                console.log(`Reading Players`);
+                let pid = document.getElementById("31").value;
+                // TODO: Add your code here.
+                //document.querySelector("#getone").innerHTML = "";
+                
+                fetch("http://localhost:3000/api/DatabaseConnection/:id/?" + "playerid="+ pid).then(response => response.json()).then(data => {
+                    let i = 0
+                //     while (i < words.length) {
+                //         console.log(words);
+                //     }
+                    //console.log(words);
+                    //var array = words[0];
+                    //document.getElementById("display-array").textContent = array.Name +" "+ array.Height +" "+ array.Weight;
+                    var table = document.getElementById('tableBody2');
+                    table.innerHTML ='';
+console.log(data);
+let dataHtml = ''
+data.forEach(element => {
+        let hof = "No"
+        if (element['HOF']){
+                hof = "Yes"
+        }
+        dataHtml += `<tr><td>${element['Name']}</td><td>${Math.floor(element['Height'] / 12)}' ${element['Height'] - Math.floor(element['Height'] / 12)*12}"</td><td>${element['Weight']}</td><td>${element['Position']}</td><td>${hof}</td></tr>`
+});
+table.innerHTML = dataHtml;
+                })
+                // console.log(words);
+                // Hint for something you will need later in the process (after backend call(s))
+                
+            
+                    
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         rhit.AdminController = class {
                 constructor() {
                 console.log("construct");
@@ -22,6 +117,10 @@ let allEntries = fetch(apiURL)
                         document.querySelector("#create").onclick = (event) => {
                                 this.createone();
                         };
+                        document.querySelector("#compare").onclick = (event) => {
+                                this.readcompareone();
+                                this.readcomparetwo();
+                        }
             }
             readAll() {
                 console.log(`Reading Players`);
@@ -85,6 +184,7 @@ table.innerHTML = dataHtml;
                 // Hint for something you will need later in the process (after backend call(s))
                 
             }
+
 
             deleteone(){
                 console.log(`Deleting Player`);
@@ -194,11 +294,21 @@ table.innerHTML = dataHtml;
 //         .then(data => console.log(data));
        
 // }
+
+
+
+
+
+
+
 /* Main */
 rhit.main = function () {
-	
-new rhit.AdminController();
+if(document.querySelector("#home")){	
+new rhit.AdminController();}
 	
 };
+if(document.querySelector("#compareit")){	
+        new rhit.compareController();}
+
 
 rhit.main();
